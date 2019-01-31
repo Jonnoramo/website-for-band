5ab9e2279caba4cc88f311236750fb1b07cf98a6
Jonnoramo <Ramsden_118@hotmail.com>
2019-01-31 13:04:25 +0000

    added chat feature

---
+++ app.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    $('#sbmt').click(function(){
+      alert('Thank you for getting in touch. We will come back to you as soon as possible.')
+    });
+
+
+    $('#Monkees Cover1.jpg').on('mouseover', function () {
+        $('#description1').slideDown('slow');
+    });
+
+
+
+    $('#pic2').on('mouseover', function () {
+        $('#description2').slideDown('slow');
+    });
+
+
+
+    $('#pic3').on('mouseover', function () {
+        $('#description3').slideDown('slow');
+    });
+
+
+});
+
+
+
